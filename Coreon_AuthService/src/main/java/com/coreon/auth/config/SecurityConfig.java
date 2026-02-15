6c59935e666dbe4a32978be6891577ee6136f738
박수한 <victor1919@naver.com>
2026-02-15 14:44:59 +0900

    cors

--- Coreon_AuthService/src/main/java/com/coreon/auth/config/SecurityConfig.java
+++ Coreon_AuthService/src/main/java/com/coreon/auth/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
                         "http://127.0.0.1:5500",   
                         "http://localhost:5500",
                         "http://localhost:8080",
-                        "http://10.0.11.129:5500"
+                        "http://10.0.11.34/:5500"
                        
                 )
         );
